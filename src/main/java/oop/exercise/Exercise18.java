4d898bf443ee8bdb492c1e14a03522f5b49ca23b
joshfeds14 <joshfeds14@gmail,com>
2021-06-05 12:58:22 -0400

    Exercise 18

---
+++ src/main/java/oop/exercise/Exercise18.java
@@ -0,0 +1,33 @@
+package oop.exercise;
+import java.util.Scanner;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Joshua Federman
+Create a program that converts temperatures from Fahrenheit to Celsius or from Celsius to Fahrenheit.
+* Prompt for the starting temperature. The program should prompt for the type of conversion and then
+* perform the conversion.
+
+The formulas are
+
+C = (F − 32) × 5 / 9
+and
+
+F = (C × 9 / 5) + 32
+Example Output
+Press C to convert from Fahrenheit to Celsius.
+Press F to convert from Celsius to Fahrenheit.
+Your choice: C
+Please enter the temperature in Fahrenheit: 32
+The temperature in Celsius is 0.
+
+Constraints
+Ensure that you allow upper or lowercase values for C and F.
+Use as few output statements as possible and avoid repeating output strings.
+ */
+
+public class Exercise18
+{
+    public static void main( String[] args ) {
+
+    }
+}
